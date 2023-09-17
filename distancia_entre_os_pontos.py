952900c8f1095715fb9335e3bf016e343537eeac
GilbertoCNetto <gilbertocurynetto@hotmail.com>
2023-09-17 01:02:25 -0300

    first commit

--- distancia_entre_os_pontos.py
+++ distancia_entre_os_pontos.py
@@ -1,7 +1,7 @@
 from math import sqrt
-xa = float(input("Digite o valor X de A:"))
-xb = float(input("Digite o valor X de B:"))
-ya = float(input("Digite o valor Y de A:"))
-yb = float(input("Digite o valor Y de B:"))
+xa = float(input("Digite o valor X do ponto A:"))
+xb = float(input("Digite o valor X do ponto B:"))
+ya = float(input("Digite o valor Y do ponto A:"))
+yb = float(input("Digite o valor Y do ponto B:"))
 distancia_entre_os_pontos = sqrt((xa - xb)**2 + (ya - yb)**2)
 print(f"A distância entre esses pontos é de {distancia_entre_os_pontos :.2f} Dab")
